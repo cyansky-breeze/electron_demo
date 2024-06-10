9d45dd8a4d05c1b34da4b8b3b0a140b7eb546ea6
cyansky-breeze <1908301384@qq.com>
2024-06-11 00:33:28 +0800

    [electron_demo] hello electron实现打包

--- code/Hello_Electron/命令.sh
+++ code/Hello_Electron/命令.sh
@@ -1,5 +1,12 @@
 
-
-
+# 生成nodejs基本项目模板
+npm init
+# 安装electron框架
 npm install electron --save-dev
 npm uninstall electron --save-dev
+# 启动
+npm run start
+# 实现安装
+npm install --save-dev @electron-forge/cli
+npx electron-forge import
+npm run make
